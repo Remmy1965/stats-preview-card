f41079b6177ab5353de38cca4a4f7141b883e910
Remmy <akeemquadri00@gmail.com>
2023-03-28 12:27:13 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        veryDarkBlue: 'hsl(233, 47%, 7%)', //main background
+        darkDesaturatedBlue: 'hsl(244, 38%, 16%)', //card background
+        softViolet: 'hsl(277, 64%, 61%)', //accent
+        white: 'hsl(0, 0%, 100%)', //main heading
+        slightlyTransWhite1: 'hsla(0, 0%, 100%, 0.75)', //main paragraph
+        slightlyTransWhite2: 'hsla(0, 0%, 100%, 0.6)', //stats heading
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans serif'],
+        deca: ['Lexend Deca', 'sans serif'],
+      },
+    },
+  },
+  plugins: [],
+};
